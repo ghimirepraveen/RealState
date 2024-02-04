39bd9ea51e2c3086254b0cf133625c2ab72336f3
ghimirepraveen <ghimireprabin7777@gmail.com>
2024-02-04 18:54:15 +0545

    adding signup with http provlen in proxy

--- api/index.js
+++ api/index.js
@@ -1,12 +1,16 @@
 import express from "express";
 //const express = require("express");
 const app = express();
+
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import "dotenv/config";
-
+import cookieParser from "cookie-parser";
 app.use(express.json());
+app.use(cookieParser());
+import User from "./model/user.model.js";
+import { error } from "console";
 mongoose
   .connect(process.env.Db_connect)
   .then(() => {
@@ -17,11 +21,18 @@ mongoose
   });
 
 app.use("/api/user", userRouter);
-app.use("/api/user", authRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
-
-// B6KB7RACu0ibdD7A;
-// mongodb+srv://ghimireprabin777:<password>@realstate.0caugqx.mongodb.net/?retryWrites=true&w=majority
---
+++ api/utils/error.js
@@ -0,0 +1,6 @@
+export const errorhandeler = (statusCode, message) => {
+  const error = new Error();
+  error.statusCode = statusCode;
+  error.message = message;
+  return error;
+};
